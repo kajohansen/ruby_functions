009adf6d0296d6b0b70458edb1872e64a0089953
Karl Andreas <kajohansen.1@gmail.com>
2016-06-17 11:56:32 +0200

    Added geocoder for finding Lat Lng by address

--- geocoder.rb
+++ geocoder.rb
@@ -3,6 +3,19 @@
 require 'geocoder'
 require 'pp'
 
-location = Geocoder.search("Waldemar Aunes vei 9a, Trondheim")
+#location = Geocoder.search("Waldemar Aunes vei 9a, Trondheim")
+#result = location[0]
 #puts location.inspect
-pp(location)
+#pp(location)
+#puts result.formatted_address
+
+search_string = ""
+ARGV.each { |val| search_string += "#{val} " }
+
+location = Geocoder.search(search_string)
+result = location[0]
+puts result.formatted_address
+
+#puts search_string
+puts 'Lat ' + result.geometry['location']['lat'].to_s
+puts 'Lng ' + result.geometry['location']['lng'].to_s
